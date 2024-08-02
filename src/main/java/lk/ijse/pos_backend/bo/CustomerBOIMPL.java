3073d2dfcf0385954c3ec49fb5fda8c226b0d45b
sachiniwathudura <sachiniwathudura@gmail.com>
2024-08-02 20:12:41 +0530

    bug fix CustomerBO ,CustomerBOIMPL

--- src/main/java/lk/ijse/pos_backend/bo/CustomerBOIMPL.java
+++ src/main/java/lk/ijse/pos_backend/bo/CustomerBOIMPL.java
@@ -1,4 +1,56 @@
 package lk.ijse.pos_backend.bo;
 
-public class CustomerBOIMPL {
+import lk.ijse.pos_backend.dao.CustomerDAO;
+import lk.ijse.pos_backend.dao.CustomerDAOIMPL;
+import lk.ijse.pos_backend.dto.CustomerDTO;
+import lk.ijse.pos_backend.entity.Customer;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class CustomerBOIMPL implements CustomerBO{
+
+    CustomerDAO customerDAO = (CustomerDAO) new CustomerDAOIMPL();
+    @Override
+    public boolean saveCustomer(Connection connection, CustomerDTO dto) throws SQLException, ClassNotFoundException {
+        return customerDAO.save(connection,new Customer(dto.getId(), dto.getName(), dto.getAddress(), dto.getSalary()));
+    }
+
+    @Override
+    public ArrayList<CustomerDTO> getAllCustomers(Connection connection) throws SQLException, ClassNotFoundException {
+        ArrayList<Customer> customerArrayList = customerDAO.getAll(connection);
+        ArrayList<CustomerDTO> customerDTOArrayList = new ArrayList<>();
+        for (Customer customer:customerArrayList) {
+            CustomerDTO dto = new CustomerDTO(
+                    customer.getId(),
+                    customer.getName(),
+                    customer.getAddress(),
+                    customer.getSalary()
+                    );
+            customerDTOArrayList.add(dto);
+        }
+        return customerDTOArrayList;
+    }
+
+    @Override
+    public CustomerDTO getCustomerById(Connection connection, String id) throws SQLException, ClassNotFoundException {
+        Customer customer = customerDAO.search(connection,id);
+        return new CustomerDTO(
+                customer.getId(),
+                customer.getName(),
+                customer.getAddress(),
+                customer.getSalary()
+        );
+    }
+
+    @Override
+    public boolean updateCustomer(Connection connection, CustomerDTO dto) throws SQLException, ClassNotFoundException {
+        return customerDAO.update(connection,new Customer(dto.getId(),dto.getName(), dto.getAddress(), dto.getSalary()));
+    }
+
+    @Override
+    public boolean deleteCustomer(Connection connection, String id) throws SQLException, ClassNotFoundException {
+        return customerDAO.delete(connection,id);
+    }
 }
