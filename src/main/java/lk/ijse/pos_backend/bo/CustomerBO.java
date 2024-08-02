2da8c4fce0cf67062d5b7e100c673a80dcd3d614
sachiniwathudura <sachiniwathudura@gmail.com>
2024-08-02 20:12:35 +0530

    bug fix CustomerBO ,CustomerBOIMPL

--- src/main/java/lk/ijse/pos_backend/bo/CustomerBO.java
+++ src/main/java/lk/ijse/pos_backend/bo/CustomerBO.java
@@ -1,4 +1,19 @@
 package lk.ijse.pos_backend.bo;
 
-public interface CustomerBO {
+import lk.ijse.pos_backend.dto.CustomerDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface CustomerBO extends SuperBO{
+    boolean saveCustomer(Connection connection, CustomerDTO customerDTO) throws SQLException, ClassNotFoundException;
+
+    ArrayList<CustomerDTO> getAllCustomers(Connection connection) throws SQLException, ClassNotFoundException;
+
+    CustomerDTO getCustomerById(Connection connection, String id) throws SQLException, ClassNotFoundException;
+
+    boolean updateCustomer(Connection connection, CustomerDTO customerDTO) throws SQLException, ClassNotFoundException;
+
+    boolean deleteCustomer(Connection connection, String id) throws SQLException, ClassNotFoundException;
 }
