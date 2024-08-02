5e606570e3e84503cf376db4af2f956a2ffc1cce
sachiniwathudura <sachiniwathudura@gmail.com>
2024-08-02 20:11:32 +0530

    create CrudUtil

---
+++ src/main/java/lk/ijse/pos_backend/dao/CrudUtil.java
@@ -0,0 +1,22 @@
+package lk.ijse.pos_backend.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CrudUtil {
+    public static <T> T execute (Connection connection, String sql, Object... args) throws SQLException{
+        PreparedStatement pstm = connection.prepareStatement(sql); // DbConnection class is that not using ,using to web app web-inf is this connected to db connection
+
+        for (int i=0; i<args.length; i++){
+            pstm.setObject((i+1),args[i]);
+        }
+
+        if (sql.startsWith("SELECT") || sql.startsWith("select")){
+            return (T) pstm.executeQuery(); // result set ek
+        }
+
+        return (T) (Boolean) (pstm.executeUpdate() > 0); //premitive =>>> boolean value
+    }
+}
+
