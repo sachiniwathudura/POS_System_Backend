7125a1373ad7fc246aaa7b98c81732ad4c8264ba
sachiniwathudura <sachiniwathudura@gmail.com>
2024-08-02 20:12:58 +0530

    bug fix CustomerDAO ,CustomerDAOIMPL

--- src/main/java/lk/ijse/pos_backend/dao/CustomerDAO.java
+++ src/main/java/lk/ijse/pos_backend/dao/CustomerDAO.java
@@ -1,4 +1,11 @@
 package lk.ijse.pos_backend.dao;
 
-public interface CustomerDAO {
+import lk.ijse.pos_backend.dto.CustomerDTO;
+import lk.ijse.pos_backend.entity.Customer;
+
+import java.sql.Connection;
+
+public interface CustomerDAO extends CrudDAO<Customer>{
+
 }
+
