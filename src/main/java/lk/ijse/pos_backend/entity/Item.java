161aa38f5473f63d88e952f3fb6e98210acea3f5
sachiniwathudura <sachiniwathudura@gmail.com>
2024-08-08 15:44:05 +0530

    upade item entity

--- src/main/java/lk/ijse/pos_backend/entity/Item.java
+++ src/main/java/lk/ijse/pos_backend/entity/Item.java
@@ -2,16 +2,21 @@ package lk.ijse.pos_backend.entity;
 
 import lombok.*;
 
+import java.math.BigDecimal;
+
 @NoArgsConstructor
 @Getter
 @Setter
 @Data
 @AllArgsConstructor
+@ToString
 
 public class Item {
-    private String ItemId;
-    private String ItemName;
-    private int ItemQty;
-    private double ItemPrice;
+    private String code;
+    private String name;
+    private int qty;
+    private BigDecimal price;
+
+
 
 }
