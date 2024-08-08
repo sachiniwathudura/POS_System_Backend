822ade503d15225fb6ebe7f04bb4c9c41e880137
sachiniwathudura <sachiniwathudura@gmail.com>
2024-08-08 15:43:38 +0530

    update db.sql

--- src/main/java/lk/ijse/pos_backend/db.sql
+++ src/main/java/lk/ijse/pos_backend/db.sql
@@ -13,9 +13,33 @@ CREATE TABLE customer (
 );
 
 
-CREATE TABLE item (
-                      ItemId VARCHAR(255) PRIMARY KEY,
-                      ItemName VARCHAR(255) NOT NULL,
-                      ItemQty INT,
-                      ItemPrice DECIMAL(10, 2)
+create table item(
+                     code VARCHAR(15) PRIMARY KEY,
+                     name VARCHAR(255) NOT NULL,
+                     qty INT NOT NULL,
+                     price DECIMAL(10,2) NOT NULL
+
 );
+
+
+create table orders(
+                       order_id VARCHAR(15) PRIMARY KEY,
+                       date DATE NOT NULL,
+                       cust_id VARCHAR(15) NOT NULL,
+                       discount DECIMAL(4,1),
+                       total DECIMAL(10,2) NOT NULL,
+                       CONSTRAINT FOREIGN KEY (cust_id) REFERENCES customer(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+
+CREATE TABLE order_details (
+                               order_id VARCHAR(15) NOT NULL,
+                               item_code VARCHAR(15) NOT NULL,
+                               unit_price DECIMAL(10, 2) NOT NULL,
+                               qty INT,
+                               PRIMARY KEY (order_id, item_code),
+                               CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE ON UPDATE CASCADE,
+                               CONSTRAINT fk_item FOREIGN KEY (item_code) REFERENCES item(code) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
