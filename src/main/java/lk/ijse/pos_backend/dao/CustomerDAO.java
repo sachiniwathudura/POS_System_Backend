9ef5f94dd09a0ce61bdaf1826f6d38c4d0af245e
sachiniwathudura <sachiniwathudura@gmail.com>
2024-08-08 15:41:58 +0530

    update customerDAO , CustomerDAOIMPL

--- src/main/java/lk/ijse/pos_backend/dao/CustomerDAO.java
+++ src/main/java/lk/ijse/pos_backend/dao/CustomerDAO.java
@@ -4,8 +4,9 @@ import lk.ijse.pos_backend.dto.CustomerDTO;
 import lk.ijse.pos_backend.entity.Customer;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public interface CustomerDAO extends CrudDAO<Customer>{
-
+    boolean exists(Connection connection, String custId) throws SQLException;
 }
 
--- src/main/java/lk/ijse/pos_backend/dao/impl/CustomerDAOIMPL.java
+++ src/main/java/lk/ijse/pos_backend/dao/impl/CustomerDAOIMPL.java
@@ -5,6 +5,7 @@ import lk.ijse.pos_backend.dao.CustomerDAO;
 import lk.ijse.pos_backend.entity.Customer;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -38,7 +39,7 @@ public final class CustomerDAOIMPL implements CustomerDAO {
 
     @Override
     public boolean update(Connection connection, Customer entity) throws SQLException, ClassNotFoundException {
-        String sql = "UPDATE customer SET name=?,address=?,salary=? WHERE id=?";
+        String sql = "UPDATE customer SET name = ?, address = ?,salary = ? WHERE id = ?";
         return CrudUtil.execute(connection,sql,entity.getName(),entity.getAddress(),entity.getSalary(),entity.getId());
     }
 
@@ -57,9 +58,18 @@ public final class CustomerDAOIMPL implements CustomerDAO {
             customer.setId(rst.getString(1));
             customer.setName(rst.getString(2));
             customer.setAddress(rst.getString(3));
-            customer.setSalary(Double.valueOf(rst.getDouble(4)));
+            customer.setSalary(Double.valueOf(rst.getString(4)));
         }
         return customer;
         }
+    public boolean exists(Connection connection, String custId) throws SQLException {
+        String query = "SELECT 1 FROM customer WHERE id = ?";
+        try (PreparedStatement pst = connection.prepareStatement(query)) {
+            pst.setString(1, custId);
+            try (ResultSet rs = pst.executeQuery()) {
+                return rs.next();
+            }
+        }
+    }
 
 }
